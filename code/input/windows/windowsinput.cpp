c8dc339a38b23be929658040e0cebd9bfbd566ea
evolver <christian-bleicher@freenet.de>
2023-03-10 22:17:55 +0100

    input:   - proper fallback to keyboard input for gamepad   - fix left- and right-distinguishing for shift, control and alt

--- code/input/windows/windowsinput.cpp
+++ code/input/windows/windowsinput.cpp
@@ -10,8 +10,6 @@
 #include <Dbt.h>
 #include <windowsx.h>
 
-static const float ANALOG_STICK_EMULATED_DEFAULT_VALUE = 1.0f;
-static ce_gamepad_state gamepad_state = {0U, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 static bool gamepad_check_availability = true;
 static bool gamepad_connected = false;
 
@@ -34,16 +32,25 @@ ce_input_update_keyboard_state(WPARAM w_param, unsigned char state)
         case VK_TAB:        keyboard_state[KEY_TAB] = state; break;
         case VK_CLEAR:      keyboard_state[KEY_CLEAR] = state; break;
         case VK_RETURN:     keyboard_state[KEY_RETURN] = state; break;
-        case VK_SHIFT:      keyboard_state[KEY_RSHIFT] = state; keyboard_state[KEY_LSHIFT] = state; break;
-        case VK_CONTROL:    keyboard_state[KEY_RCONTROL] = state; keyboard_state[KEY_LCONTROL] = state; break;
-        case VK_MENU:       keyboard_state[KEY_RALT] = state; keyboard_state[KEY_LALT] = state; break;
+        case VK_SHIFT:
+            keyboard_state[KEY_LSHIFT] = (GetAsyncKeyState(VK_LSHIFT) & 0x8000) != 0;
+            keyboard_state[KEY_RSHIFT] = (GetAsyncKeyState(VK_RSHIFT) & 0x8000) != 0;
+            break;
+        case VK_CONTROL:
+            keyboard_state[KEY_LCONTROL] = (GetAsyncKeyState(VK_LCONTROL) & 0x8000) != 0;
+            keyboard_state[KEY_RCONTROL] = (GetAsyncKeyState(VK_RCONTROL) & 0x8000) != 0;
+            break;
+        case VK_MENU:
+            keyboard_state[KEY_LALT] = (GetAsyncKeyState(VK_LMENU) & 0x8000) != 0;
+            keyboard_state[KEY_RALT] = (GetAsyncKeyState(VK_RMENU) & 0x8000) != 0;
+            break;
         case VK_PAUSE:      keyboard_state[KEY_PAUSE] = state; break;
         case VK_CAPITAL:    keyboard_state[KEY_CAPS_LOCK] = state; break;
         case VK_ESCAPE:     keyboard_state[KEY_ESCAPE] = state; break;
-        case VK_LEFT:       keyboard_state[KEY_LEFT] = state; gamepad_state.left_stick_x = (state == 1U) ? -ANALOG_STICK_EMULATED_DEFAULT_VALUE : 0.0f; break;
-        case VK_RIGHT:      keyboard_state[KEY_RIGHT] = state; gamepad_state.left_stick_x = (state == 1U) ? ANALOG_STICK_EMULATED_DEFAULT_VALUE : 0.0f; break;
-        case VK_UP:         keyboard_state[KEY_UP] = state; gamepad_state.left_stick_y = (state == 1U) ? ANALOG_STICK_EMULATED_DEFAULT_VALUE : 0.0f; break;
-        case VK_DOWN:       keyboard_state[KEY_DOWN] = state; gamepad_state.left_stick_y = (state == 1U) ? -ANALOG_STICK_EMULATED_DEFAULT_VALUE : 0.0f; break;
+        case VK_LEFT:       keyboard_state[KEY_LEFT] = state; break;
+        case VK_RIGHT:      keyboard_state[KEY_RIGHT] = state; break;
+        case VK_UP:         keyboard_state[KEY_UP] = state; break;
+        case VK_DOWN:       keyboard_state[KEY_DOWN] = state; break;
         case VK_SPACE:      keyboard_state[KEY_SPACE] = state; break;
         case VK_PRIOR:      keyboard_state[KEY_PAGE_UP] = state; break;
         case VK_NEXT:       keyboard_state[KEY_PAGE_DOWN] = state; break;
@@ -134,12 +141,6 @@ ce_input_update_keyboard_state(WPARAM w_param, unsigned char state)
         case VK_F24:        keyboard_state[KEY_F24] = state; break;
         case VK_NUMLOCK:    keyboard_state[KEY_NUMLOCK] = state; break;
         case VK_SCROLL:     keyboard_state[KEY_SCROLL] = state; break;
-        case VK_LSHIFT:     keyboard_state[KEY_LSHIFT] = state; break;
-        case VK_RSHIFT:     keyboard_state[KEY_RSHIFT] = state; break;
-        case VK_LCONTROL:   keyboard_state[KEY_LCONTROL] = state; break;
-        case VK_RCONTROL:   keyboard_state[KEY_RCONTROL] = state; break;
-        case VK_LMENU:      keyboard_state[KEY_LALT] = state; break;
-        case VK_RMENU:      keyboard_state[KEY_RALT] = state; break;
     }
 }
 
@@ -268,9 +269,101 @@ ce_input_gamepad_get_state(ce_gamepad_state* const gps)
     }
     else
     {
-        // wip: fallback to keyboard input
-        gps->left_stick_x = gamepad_state.left_stick_x;
-        gps->left_stick_y = gamepad_state.left_stick_y;
+        // fallback to keyboard input
+        // Buttons
+        if (1U == keyboard_state[KEY_F]) gps->buttons |= GAMEPAD_DPAD_UP;
+        if (1U == keyboard_state[KEY_V]) gps->buttons |= GAMEPAD_DPAD_DOWN;
+        if (1U == keyboard_state[KEY_C]) gps->buttons |= GAMEPAD_DPAD_LEFT;
+        if (1U == keyboard_state[KEY_B]) gps->buttons |= GAMEPAD_DPAD_RIGHT;
+        if (1U == keyboard_state[KEY_F2]) gps->buttons |= GAMEPAD_START;
+        if (1U == keyboard_state[KEY_F1]) gps->buttons |= GAMEPAD_BACK;
+        if (1U == keyboard_state[KEY_LSHIFT]) gps->buttons |= GAMEPAD_LEFT_THUMB;
+        if (1U == keyboard_state[KEY_RSHIFT]) gps->buttons |= GAMEPAD_RIGHT_THUMB;
+        if (1U == keyboard_state[KEY_LCONTROL]) gps->buttons |= GAMEPAD_LEFT_SHOULDER;
+        if (1U == keyboard_state[KEY_RCONTROL]) gps->buttons |= GAMEPAD_RIGHT_SHOULDER;
+        if (1U == keyboard_state[KEY_K]) gps->buttons |= GAMEPAD_A;
+        if (1U == keyboard_state[KEY_L]) gps->buttons |= GAMEPAD_B;
+        if (1U == keyboard_state[KEY_J]) gps->buttons |= GAMEPAD_X;
+        if (1U == keyboard_state[KEY_I]) gps->buttons |= GAMEPAD_Y;
+
+        // Analog sticks
+        if (1U == keyboard_state[KEY_A])
+        {
+            gps->left_stick_x = -1.0f;
+        }
+        else if (1U == keyboard_state[KEY_D])
+        {
+            gps->left_stick_x = 1.0f;
+        }
+        if (1U == keyboard_state[KEY_W])
+        {
+            gps->left_stick_y = 1.0f;
+        }
+        else if (1U == keyboard_state[KEY_S])
+        {
+            gps->left_stick_y = -1.0f;
+        }
+        if (1U == keyboard_state[KEY_A] && 1U == keyboard_state[KEY_W])
+        {
+            gps->left_stick_x = -0.7f;
+            gps->left_stick_y = 0.7f;
+        }
+        else if (1U == keyboard_state[KEY_D] && 1U == keyboard_state[KEY_W])
+        {
+            gps->left_stick_x = 0.7f;
+            gps->left_stick_y = 0.7f;
+        }
+        else if (1U == keyboard_state[KEY_A] && 1U == keyboard_state[KEY_S])
+        {
+            gps->left_stick_x = -0.7f;
+            gps->left_stick_y = -0.7f;
+        }
+        else if (1U == keyboard_state[KEY_D] && 1U == keyboard_state[KEY_S])
+        {
+            gps->left_stick_x = 0.7f;
+            gps->left_stick_y = -0.7f;
+        }
+
+        if (1U == keyboard_state[KEY_LEFT])
+        {
+            gps->right_stick_x = -1.0f;
+        }
+        else if (1U == keyboard_state[KEY_RIGHT])
+        {
+            gps->right_stick_x = 1.0f;
+        }
+        if (1U == keyboard_state[KEY_UP])
+        {
+            gps->right_stick_y = 1.0f;
+        }
+        else if (1U == keyboard_state[KEY_DOWN])
+        {
+            gps->right_stick_y = -1.0f;
+        }
+        if (1U == keyboard_state[KEY_LEFT] && 1U == keyboard_state[KEY_UP])
+        {
+            gps->right_stick_x = -0.7f;
+            gps->right_stick_y = 0.7f;
+        }
+        else if (1U == keyboard_state[KEY_RIGHT] && 1U == keyboard_state[KEY_UP])
+        {
+            gps->right_stick_x = 0.7f;
+            gps->right_stick_y = 0.7f;
+        }
+        else if (1U == keyboard_state[KEY_LEFT] && 1U == keyboard_state[KEY_DOWN])
+        {
+            gps->right_stick_x = -0.7f;
+            gps->right_stick_y = -0.7f;
+        }
+        else if (1U == keyboard_state[KEY_RIGHT] && 1U == keyboard_state[KEY_DOWN])
+        {
+            gps->right_stick_x = 0.7f;
+            gps->right_stick_y = -0.7f;
+        }
+
+        // Trigger
+        if (1U == keyboard_state[KEY_M]) gps->left_trigger = 1.0f;
+        if (1U == keyboard_state[KEY_DIVIDE]) gps->right_trigger = 1.0f;
     }
 }
 
