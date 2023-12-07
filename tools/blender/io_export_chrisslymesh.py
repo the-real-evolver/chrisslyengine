5bd664b01f9712072228e28abab3dc8382f34353
evolver <christian-bleicher@freenet.de>
2023-12-07 20:48:03 +0100

    tools (Blender Add-on):   - disable depth_write when pass uses alpha blending (so the engine can render transparent objects correctly)

--- tools/blender/io_export_chrisslymesh.py
+++ tools/blender/io_export_chrisslymesh.py
@@ -142,8 +142,9 @@ def ce_write_material(file_path, materials):
         file.write("    pass\n")
         file.write("    {\n")
         file.write("        lighting on\n")
-        if mat.blend_method == 'BLEND':
+        if mat.blend_method == 'BLEND' or mat.blend_method == 'CLIP':
             file.write("        scene_blend src_alpha one_minus_src_alpha\n")
+            file.write("        depth_write off\n")
         file.write("        diffuse %f %f %f %f\n" % (mat.diffuse_color[0], mat.diffuse_color[1], mat.diffuse_color[2], mat.diffuse_color[3]))
         file.write("        specular %f %f %f 1.0 %f\n" % (mat.specular_color[0], mat.specular_color[1], mat.specular_color[2], mat.specular_intensity * 64.0))
         file.write("        cull_hardware %s\n" % ('clockwise' if mat.use_backface_culling else 'none'))
